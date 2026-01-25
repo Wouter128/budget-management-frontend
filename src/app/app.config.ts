66161b35756f60ed3fed962476f2b546656549cc
Wouter Biront <wouterbiront@gmail.com>
2026-01-25 21:23:09 +0100

    Initial commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,53 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {
+  AutoRefreshTokenService,
+  createInterceptorCondition,
+  INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+  IncludeBearerTokenCondition,
+  includeBearerTokenInterceptor,
+  provideKeycloak,
+  UserActivityService,
+  withAutoRefreshToken
+} from 'keycloak-angular';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([includeBearerTokenInterceptor])),
+    provideKeycloak({
+      config: {
+        url: "http://localhost:8080",
+        realm: "budget-management",
+        clientId: "budget-management-client"
+      },
+      initOptions: {
+        onLoad: "check-sso",
+        silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html'
+      },
+      features: [
+        withAutoRefreshToken({
+          onInactivityTimeout: 'logout',
+          sessionTimeout: 1000
+        })
+      ],
+      providers: [
+        AutoRefreshTokenService,
+        UserActivityService,
+        {
+          provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+          useValue: [
+            createInterceptorCondition<IncludeBearerTokenCondition>({
+              urlPattern: /^(http:\/\/localhost:8081)(\/.*)?$/i
+            })
+          ]
+        }
+      ]
+    })
+  ]
+};
